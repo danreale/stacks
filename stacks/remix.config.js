6535904c6678650bb8d2716ba1c59d5d72503fb9
Dan Reale <realed@dnb.com>
2023-10-09 12:08:27 -0400

    initial commit with structure

---
+++ stacks/remix.config.js
@@ -0,0 +1,14 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+export default {
+  ignoredRouteFiles: ["**/.*"],
+  server:
+    process.env.NETLIFY || process.env.NETLIFY_LOCAL
+      ? "./server.ts"
+      : undefined,
+  serverBuildPath: ".netlify/functions-internal/server.js",
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // publicPath: "/build/",
+  // serverBuildPath: "build/index.js",
+  tailwind: true,
+};
