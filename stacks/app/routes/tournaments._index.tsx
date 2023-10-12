e61b1b42dfafac61e652b016a85dab24ecba23e0
Dan Reale <realed@dnb.com>
2023-10-12 11:13:10 -0400

    fixed formatting

--- stacks/app/routes/tournaments._index.tsx
+++ stacks/app/routes/tournaments._index.tsx
@@ -1,7 +1,9 @@
 export default function Tournaments() {
   return (
     <>
-      <div>Tournaments Page Coming Soon</div>
+      <div className="flex justify-center text-center text-2xl">
+        Tournaments Page Coming Soon
+      </div>
     </>
   );
 }
